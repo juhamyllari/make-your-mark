aa64ad8bb78f77f208f27538457e5ac11fc9fcd7
Juha Myllari <jmyllari@gmail.com>
2018-12-02 18:34:34 +0200

    separate menu items for hide/show read

---
+++ src/main/java/Menu/HideRead.java
@@ -0,0 +1,19 @@
+package Menu;
+
+import IO.IO;
+import bookmark.BookmarkContainer;
+
+public class HideRead extends MenuItem {
+
+    public HideRead() {
+        super("hide read bookmarks");
+        addKey("hide");
+        addKey("h");
+    }
+
+    @Override
+    public void execute(BookmarkContainer container, IO io) {
+        container.setShowingRead(false);
+    }
+
+}
--- src/main/java/Menu/ShowRead.java
+++ src/main/java/Menu/ShowRead.java
@@ -6,13 +6,13 @@ import bookmark.BookmarkContainer;
 public class ShowRead extends MenuItem {
 
     public ShowRead() {
-        super("show/hide (marked as) read bookmarks");
+        super("show read bookmarks");
         addKey("show");
         addKey("sh");
     }
 
     @Override
     public void execute(BookmarkContainer container, IO io) {
-        container.setShowingRead(!container.isShowingRead());
+        container.setShowingRead(true);
     }
 }

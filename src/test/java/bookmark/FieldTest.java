756c5a03fb2d1047ef586010f3f39d90712988d5
Jetro Hulkko <jetro.hulkko@helsinki.fi>
2018-12-10 21:22:04 +0200

    import added so maybe no error

--- src/test/java/bookmark/FieldTest.java
+++ src/test/java/bookmark/FieldTest.java
@@ -3,6 +3,7 @@ package bookmark;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
